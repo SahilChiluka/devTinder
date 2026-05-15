b66912ce6cd165bf766e73118460acc076a3790d
sahil chiluka <sahilchiluka10@gmail.com>
2026-05-15 21:07:16 +0530

    exploring get, post and delete methods

--- src/app.js
+++ src/app.js
@@ -2,16 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello from the server!");
-});
-
-app.use("/test", (req, res) => {
-  res.send("You are on a test page.");
-});
-
 app.use("/", (req, res) => {
   res.send("Welcome to Home Page!");
+});
+
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({firstName: "Sahil", lastName: "Chiluka"});
+});
+
+app.post("/user", (req, res) => {
+  console.log("Saving User into Database");
+  res.send("User Saved Successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted User Successfully");
+});
+
+// If you do "app.use" This will match all the HTTP methods (GET, POST, DELETE, etc) to /test
+app.use("/test", (req, res) => {
+  res.send("You are on a test page.");
 });
 
 app.listen(7777, () => {
